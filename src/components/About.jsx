b8204ace85f14dc769e2e11223dbecf92e7409d0
prafful9111 <100062223+prafful9111@users.noreply.github.com>
2024-10-16 13:46:22 +0530

    modified About

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -49,7 +49,7 @@ const About = () => {
         variants={fadeIn("", "", 0.1, 1)}
         className='mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]'
       >
-        I'm a skilled software developer with experience in JavaScript and JAVA Programming language, and expertise in frameworks like React and Node.js. I'm a quick learner and adaptable in nature, to create efficient, scalable, and user-friendly solutions that solve real-world problems. Let's work together to bring your ideas to life!
+        I'm a skilled software developer with experience in JavaScript and JAVA Programming language, and expertise in frameworks like React and Node.js. I'm a quick learner and adaptable in nature, to create efficient, scalable, and user-friendly solutions that solve real-world problems. Let's work together to bring your ideas to life! 
       </motion.p>
 
       <div className='mt-20 flex flex-wrap gap-10'>
